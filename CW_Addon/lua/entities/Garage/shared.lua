46387ef93fed534363115c858a9979013ba13743
Skyreth <63458729+Skyreth@users.noreply.github.com>
2021-04-26 22:08:58 +0200

    Update shared.lua

--- CW_Addon/lua/entities/Garage/shared.lua
+++ CW_Addon/lua/entities/Garage/shared.lua
@@ -1,12 +1,13 @@
-ENT.BASE = "base_ia"
-ENT.TYPE = "ai"
+ENT.Base = "anim"
+ENT.Type = "base_gmodentity"
+
 ENT.PrintName = "Npc Garage"
 ENT.Author = "Skyreth"
 ENT.Category = "NPC Skyreth"
 ENT.Contact = "N/A"
 ENT.Purpose = "N/A"
 ENT.Instructions = "Press E (Use touch)"
-ENT.Spawnable = false
+ENT.Spawnable = true
 ENT.AdminSpawnable = true
 ENT.AutomaticFrameAdvance = true
 
